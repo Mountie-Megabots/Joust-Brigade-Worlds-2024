418513c79422cb323f0a4836343b6527694e47f7
Medieval Apple <114710036+MedievalApple@users.noreply.github.com>
2024-04-17 00:18:03 -0500

    Fixed Var Name Issue

--- assets/js/VisitApiRequest.js
+++ assets/js/VisitApiRequest.js
@@ -1,10 +1,8 @@
-const host = "https://worlds.medievalapple.workers.dev/";
-
 console.log(MakeVisitApiRequest());
 
 async function MakeVisitApiRequest()
 {
-    const response = await fetch(host, {
+    const response = await fetch("https://worlds.medievalapple.workers.dev/", {
         method: "GET", // *GET, POST, PUT, DELETE, etc.
         mode: "cors", // no-cors, *cors, same-origin
         cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
